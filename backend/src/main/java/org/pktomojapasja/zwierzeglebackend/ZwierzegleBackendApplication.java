a9ecf9636a7be362d99ed0daf3db643f36b57bcf
Karol Nowacki <karol@b24net.pl>
2022-12-11 15:38:29 +0100

    init

---
+++ backend/src/main/java/org/pktomojapasja/zwierzeglebackend/ZwierzegleBackendApplication.java
@@ -0,0 +1,13 @@
+package org.pktomojapasja.zwierzeglebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZwierzegleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZwierzegleBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/org/pktomojapasja/zwierzeglebackend/ZwierzegleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pktomojapasja.zwierzeglebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZwierzegleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
