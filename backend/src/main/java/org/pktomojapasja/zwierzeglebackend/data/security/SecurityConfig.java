5ff44b0a141e90747b3ebc14c730195e18643358
Radoslaw <radoslaw.mazur97@gmail.com>
2022-12-27 20:17:24 +0100

    Fixing problem with get method

--- backend/src/main/java/org/pktomojapasja/zwierzeglebackend/data/security/SecurityConfig.java
+++ backend/src/main/java/org/pktomojapasja/zwierzeglebackend/data/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package org.pktomojapasja.zwierzeglebackend.data.security;
 
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.pktomojapasja.zwierzeglebackend.data.repository.IUserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +14,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -29,16 +31,25 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.httpBasic().disable()
                 .csrf().disable()
+                .cors()
+                .and()
                 .authorizeHttpRequests()
-                .requestMatchers("/api/user/**").authenticated()
+                .requestMatchers("/api/user/**").hasRole("USER")
                 .requestMatchers("/api/auth/**").permitAll()
-                .anyRequest().permitAll()
                 .and()
                 .userDetailsService(userDetailsService)
+                .exceptionHandling()
+                .authenticationEntryPoint(
+                        (request, response, authException) ->
+                                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized")
+                )
+                .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        return http.build();
+        http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+          return http.build();
     }
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
