379a2db75fba97c4bbef2eb5e96d8672b0b8b1a8
Karol Nowacki <karol@b24net.pl>
2023-01-04 18:57:17 +0100

    fix auth endpoints

--- backend/src/main/java/org/pktomojapasja/zwierzeglebackend/api/AuthApi.java
+++ backend/src/main/java/org/pktomojapasja/zwierzeglebackend/api/AuthApi.java
@@ -7,6 +7,7 @@ import org.pktomojapasja.zwierzeglebackend.api.model.RegistrationRequest;
 import org.pktomojapasja.zwierzeglebackend.api.model.TokenResponse;
 import org.pktomojapasja.zwierzeglebackend.data.user.UserService;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,13 +18,13 @@ public class AuthApi {
     private final UserService userService;
 
     @PostMapping("/register")
-    public TokenResponse register(RegistrationRequest registrationRequest) {
+    public TokenResponse register(@RequestBody RegistrationRequest registrationRequest) {
         String token = userService.register(registrationRequest.toUser());
         return TokenResponse.of(token);
     }
 
     @PostMapping("/login")
-    public TokenResponse login(LoginRequest loginRequest) {
+    public TokenResponse login(@RequestBody LoginRequest loginRequest) {
         String token = userService.login(loginRequest);
         return TokenResponse.of(token);
     }
