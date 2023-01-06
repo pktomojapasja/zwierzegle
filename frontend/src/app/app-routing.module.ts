525aa4dd8cf970320d4647022d9d1082c71ce9ba
Zeimdall <48431176+Zeimdall@users.noreply.github.com>
2023-01-06 11:07:24 +0100

    Adding styles to frontend pages (#12)
    * Create files for logging and registering to app
    
    * add styles for login and register page
    
    * Add styles to social buttons, registering and forgot password buttons
    
    * add style to contact page
    
    Co-authored-by: Piotr <piotrwdzych@gmail.com>

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { HomeComponent } from './pages/home/home.component';
 import { BlogComponent } from './pages/blog/blog.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 const routes: Routes = [
   {path:  "", pathMatch:  "full", component: HomeComponent},
   {path: "blog", component: BlogComponent},
   {path: "login", component: LoginComponent},
   {path: "register", component: RegisterComponent},
+  {path: "contact", component: ContactComponent},
 ];
 
 @NgModule({
