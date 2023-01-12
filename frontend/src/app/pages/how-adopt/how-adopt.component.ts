24f7dd5f1f3b33762244b7c22c29faf11e8124ae
Zeimdall <48431176+Zeimdall@users.noreply.github.com>
2023-01-12 19:30:06 +0100

    Add new components and styles on frontend (#13)
    * Create files for logging and registering to app
    
    * add styles for login and register page
    
    * Add styles to social buttons, registering and forgot password buttons
    
    * add style to contact page
    
    * Add new components and styles on frontend
    
    * add how-can-you-help page and change styles on blog page
    
    * Add edit profile component and befriend component
    
    Co-authored-by: Piotr <piotrwdzych@gmail.com>

---
+++ frontend/src/app/pages/how-adopt/how-adopt.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-how-adopt',
+  templateUrl: './how-adopt.component.html',
+  styleUrls: ['./how-adopt.component.scss']
+})
+export class HowAdoptComponent {
+
+}
